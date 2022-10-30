a1331fe877115e2255bc155cebc2bd763e831f1d
Harry Buxton <92524519+hbux@users.noreply.github.com>
2022-10-30 16:09:47 +0000

    Add code comments in backend

--- backend/controllers/authenticationController.js
+++ backend/controllers/authenticationController.js
@@ -1,36 +1,46 @@
+// Third-party dependencies
 const bcrypt = require('bcrypt');
 
+// models and service dependencies
 const User = require('../models/user');
 const tokenService = require('../services/tokenService');
 
+// Login request
 exports.login = (req, res) => {
     const { email, password } = req.body;
 
+    // Validation check to ensure email and password are present
     if (!email || !password) {
         res.status(400).send({ message: 'Please enter all fields.' });
 
         return;
     }
 
+    // Attempt to find a user with a matching email
     User.findOne({
         email: email
     }).then(user => {
+        // If user could not be found return error message
         if (!user) {
             res.status(400).send({ message: 'Invalid username or password' });
 
             return;
         }
 
+        // Using third party dependency to manage password security (hashing)
         var isPasswordValid = bcrypt.compareSync(password, user.hashedPassword);
 
+        // Ensures the password matches what the users entered
         if (!isPasswordValid) {
             res.status(400).send({ message: 'Invalid username or password.' });
 
             return;
         }
 
+        // Create a JWT token
         var accessToken = tokenService.createToken(user);
 
+        // return success status with the token created
         res.status(200).send({
             username: user.email,
             access_token: accessToken
@@ -38,27 +48,33 @@ exports.login = (req, res) => {
     })
 }
 
+// Registration
 exports.register = (req, res) => {
     const { firstName, lastName, email, password, confirmPassword } = req.body;
 
+    // Validation on each parameter
     if (!firstName || !lastName || !email || !password || !confirmPassword) {
         res.status(400).send({ message: 'Please enter all fields.' });
 
         return;
     }
 
+    // Ensures passwords are the same
     if (password != confirmPassword) {
         res.status(400).send({ message: 'Passwords do not match.' });
 
         return;
     }
 
+    // Attempting to find a matching email (emails must be unique)
     User.findOne({
         email: email
     }).then(user => {
+        // If user is found, email already exists
         if (user) {
             res.send('Email already exists.');
         } else {
+            // Creating the new user and hasing the password
             const user = new User({
                 firstName: firstName,
                 lastName: lastName,
@@ -66,11 +82,14 @@ exports.register = (req, res) => {
                 hashedPassword: bcrypt.hashSync(password, 8)
             });
 
+            // Saves the user in the database
             user.save()
                 .then(data => {
+                    // Success = 200 status code
                     res.status(200).send({ message: 'Thanks. Your account has now been created.' });
                 })
                 .catch(error => {
+                    // Something went wrong with adding to the database, return bad request
                     res.status(400).send({ message: error });
                 });
         }
--- backend/services/tokenService.js
+++ backend/services/tokenService.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const securityKey = require('../config/keys').securityKey;
 
 function createToken(user) {
+    // Creates a token based on user details
     var accessToken = jwt.sign({
         id: user.id,
         email: user.email,
@@ -12,13 +13,17 @@ function createToken(user) {
 }
 
 validateToken = (req, res, next) => {
+    // This is middleware which is run after routes but before controllers
+    // which ensures the user sends a token with their request 
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
+    // If token is not present
     if (!token) {
         res.status(401).send();
     }
 
+    // Third party dependecy to validate the JWT access token
     jwt.verify(token, securityKey, (error, user) => {
         if (error) {
             res.status(403).send();
@@ -26,6 +31,7 @@ validateToken = (req, res, next) => {
 
         req.user = user;
 
+        // Next runs the next function in the http request pipeline
         next();
     });
 }
