22d8a05d77e3c96bcdf34bd975f82c79daa938f5
Harry Buxton <92524519+hbux@users.noreply.github.com>
2022-11-08 23:33:53 +0000

    Remove frontend

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Router from './router'
-import Store from './stores/index'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-createApp(App).use(Router).use(Store).mount('#app')
